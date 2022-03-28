f208339eb98532e9835b69beffb73279c500055b
Faber <a.a.r.samuel99@gmail.com>
2022-03-27 20:24:00 -0600

    updated vue and multipage

--- 05-vue/vue.config.js
+++ 05-vue/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://exusai.github.io/CursoFrontEnd/dist/'
+    ? '/CursoFrontEnd/dist/'
     : '/'
 }
